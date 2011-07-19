e0f0530233a942d2bae322f34b8ed6df8e12424a
Radu Cotescu <radu@cotescu.com>
2011-07-19 01:28:26 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en">
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Radu Cotescu's resume</title>
+	<link rel="stylesheet" href="style.css" type="text/css"/>
+	<link rel="shortcut icon" type="image/x-icon" href="http://radu.cotescu.com/favicon.ico"/>
+	<meta name="description" content="a = b + a - (b = a); I do this often. Do you want to hire me? Check my resume!"/>
+	<meta name="keywords" content="radu, cotescu, resume, cv, java, oracle, linux, ubuntu, linux, software, developer, engineer, bash, virtualization, python, c, c++"/>
+	<script type="text/javascript">
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-8815189-1']);
+	_gaq.push(['_trackPageview']);
+	_gaq.push(['_trackPageLoadTime']);
+
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+	</script>
+</head>
+<body>
+	<div class="menu">
+		<h3>Overview</h3>
+		<ul>
+			<li><a href="http://radu.cotescu.com/resume/#" title="Contact information">Contact information</a></li>
+			<li><a href="http://radu.cotescu.com/resume/#summary" title="Summary">Summary</a></li>
+			<li><a href="http://radu.cotescu.com/resume/#skills" title="Skills">Skills</a></li>
+			<li><a href="http://radu.cotescu.com/resume/#experience" title="Experience">Experience</a></li>
+			<li><a href="http://radu.cotescu.com/resume/#education" title="Education">Education</a></li>
+			<li><a href="http://radu.cotescu.com/resume/#publications" title="Publications">Publications</a></li>
+		</ul>
+	</div>
+	<div class="wrapper">
+		<div class="hresume">
+			<div class="contact vcard">
+				<div class="nameAndTitle">
+					<h1 class="fn" id="radu-cotescu">
+
+						<span class="given-name">Radu</span> <span class="family-name">Cotescu</span> 
+					</h1>
+					<h2 class="title">Software Engineer</h2>
+				</div>
+				<div class="contactData">
+					<h3>Contact information</h3>
+					<ul>
+
+						<li class="adr">
+							<span class="locality">Southampton</span>, <span class="country-name">United Kingdom</span>
+						</li>
+						<li>
+							<a class="email" href="javascript:noSpam1('radu', 'cotescu.com')" title="Email Radu Cotescu">moc.ucsetoc@udar</a>
+						</li>
+						<li>
+
+							<a class="url website" rel="me" href="http://radu.cotescu.com/" title="Radu Cotescu&#39;s professional blog">http://radu.cotescu.com</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div id="summary" class="container">
+				<h3>Summary</h3>
+				<p class="summary">
+					Software Engineer, aspiring to become an Oracle Certified Master Java SE Developer, with a passion for Java, Python, *nix and all other geek stuff out there. Passionate Linux (Ubuntu) power user, strongly commited to the open-source world.
+				</p>
+			</div>
+			<div id="skills" class="container">
+				<h3>Skills</h3>
+				<ul>
+					<li>good knowledge of <a href="http://en.wikipedia.org/wiki/Java_%28programming_language%29" rel="tag" title="Java">Java</a>, <a href="http://en.wikipedia.org/wiki/C_%28programming_language%29" rel="tag" title="C">C</a> and <a href="http://en.wikipedia.org/wiki/C%2B%2B" rel="tag" title="C++">C++</a> programming languages</li>
+					<li>experience in working with <a href="http://en.wikipedia.org/wiki/Oracle_Application_Server" title="iAS" rel="tag">Oracle Application Server</a> and <a href="http://en.wikipedia.org/wiki/TopLink" title="Oracle TopLink" rel="tag">Oracle TopLink</a> (JPA) framework</li>
+					<li>good knowledge of the <a href="http://struts.apache.org/2.x/" title="Struts2" rel="tag">Struts2</a> framework</li>
+					<li>experience in working with <a href="http://tomcat.apache.org/" title="Apache Tomcat" rel="tag">Apache Tomcat</a> servlets container</li>
+					<li>basic competences using <a href="http://www.python.org/" rel="tag" title="Python">Python</a></li>
+					<li>good <a href="http://en.wikipedia.org/wiki/Javascript" rel="tag" title="JavaScript">JavaScript</a> skills</li>
+					<li>familiar with <a href="http://jquery.com/" title="jQuery" rel="tag">jQuery</a> JavaScript framework</li>
+					<li>good <a href="http://www.w3.org/Style/CSS/" rel="tag" title="CSS">CSS</a> skills</li>
+					<li>good <a href="http://en.wikipedia.org/wiki/XHTML" rel="tag" title="HTML / XHTML">HTML / XHTML</a> skills</li>
+					<li>good <a href="http://en.wikipedia.org/wiki/Bash" rel="tag" title="bash scripting">bash scripting</a> skills</li>
+					<li>good knowledge of Linux: <a href="http://www.ubuntu.com/" rel="tag" title="Ubuntu">Ubuntu</a>, <a href="http://www.oracle.com/technologies/linux/index.html" rel="tag" title="Oracle Enterprise Linux">Oracle Enterprise Linux</a>, <a href="http://www.redhat.com/" rel="tag" title="Red Hat Enterprise Linux">Red Hat Enterprise Linux</a> and <a href="http://www.busybox.net/" rel="tag" title="BusyBox">BusyBox</a>, The Swiss Army Knife of Embedded Linux</li>
+					<li>good knowledge of using and administering <a href="http://subversion.tigris.org/" rel="tag" title="subversion">subversion</a></li>
+					<li>good knowledge of using and administering <a href="http://git-scm.com/" rel="tag" title="GIT">GIT</a> with gitosis</li>
+					<li>good knowledge of database modeling and implementation, experience with <a href="http://www.oracle.com/" rel="tag">Oracle</a>, <a href="http://www.mysql.com/" rel="tag" title="MySQL">MySQL</a> and <a href="http://www.postgresql.org/" rel="tag" title="PostgreSQL">PostgreSQL</a></li>
+					<li>good knowledge of <a href="http://en.wikipedia.org/wiki/Platform_virtualization" rel="tag" title="virtualization">virtualization</a> tools like <a href="http://www.vmware.com/products/server/" rel="tag" title="VMware Server">VMware Server</a>, <a href="http://www.vmware.com/products/player/" rel="tag" title="VMware Player">VMware Player</a>, <a href="http://www.virtualbox.org/" rel="tag" title="VirtualBox">VirtualBox</a></li>
+					<li>familiar with <a href="http://en.wikipedia.org/wiki/Model-view-controller" rel="tag" title="MVC frameworks">MVC frameworks</a> like <a href="http://djangoproject.com/" rel="tag" title="Django Python Framework">Django</a></li>
+					<li>familiar with <a href="http://en.wikipedia.org/wiki/Design_pattern_(computer_science)" rel="tag" title="design patterns">design patterns</a></li>
+					<li>fluent English speaker (8 out of 9 band score on <a href="http://www.ielts.org/" rel="tag" title="International English Language Testing System">IELTS</a>, Academic Module)</li>
+					<li>owner of a technology oriented blog</li>
+				</ul>
+			</div>
+			<div id="experience" class="container">
+				<h3>Experience</h3>
+				<ol>
+
+				   <li class="experience vevent vcard">
+						<h4 class="title">Student Software Engineer</h4>
+						<h5 class="org summary"><a href="http://www.oracle.com/" title="Oracle" class=".institution">Oracle</a></h5>
+						<p class="organization-details">(10,001 or more employees; ORCL; Public Company; Computer Software)</p>
+						<p class="period">
+							<abbr class="dtstart" title="2009-04-06">April 2009</abbr> - <abbr class="dtstamp" title="2009-10-06">October 2009</abbr>
+						</p>
+						<p class="description">
+							Part of the Service Engineering department, I was involved in developing a SOAP web services integration layer between an internal Oracle portal and My Oracle Support (the customer facing support portal) using technologies like J2SE, J2EE, Oracle Database, Oracle TopLink (ORM tool) and Oracle Application Server. 		
+						</p>
+					</li>
+				   <li class="experience vevent vcard">
+						<h4 class="title">Intern</h4>
+						<h5 class="org summary"><a href="http://www.oracle.com/" title="Oracle">Oracle</a></h5>
+						<p class="organization-details">(10,001 or more employees; ORCL; Public Company; Computer Software)</p>
+						<p class="period">
+							<abbr class="dtstart" title="2008-07-07">July 2008</abbr> - <abbr class="dtstamp" title="2008-10-06">October 2008</abbr>
+						</p>
+						<p class="description">
+							During my internship in the Siebel CRM LABs Team I have implemented, developed and administered CELWiki, a collaborative wiki that provides useful information for Service Engineers in solving Service Requests for Siebel clients. Built on top of a LAMP architecture (Linux - Oracle Enterprise Linux, Apache, MySQL and PHP), CELWiki contains tested and verified tutorials for configuring a multitude of software products like LDAPs, web servers, FTP servers, database servers, Linux environments, Windows environments, virtualization tools, etc.
+						</p>
+					</li>
+					<li class="experience vevent vcard">
+						<h4 class="title">Vice Chairman Public Relations at EESTEC Bucharest</h4>
+						<h5 class="org summary"><a href="http://www.eestec.ro/" title="EESTEC">EESTEC</a></h5>
+						<p class="organization-details">(Non-Profit Organization Management industry)</p>
+						<p class="period">
+							<abbr class="dtstart" title="2008-03-10">March 2008</abbr> - <abbr class="dtstamp" title="2008-05-19">May 2008</abbr>
+						</p>
+						<p class="description">
+							I have developed and maintained partnerships for the organization. I was also responsible for brand identity management.
+
+						</p>
+					</li>
+					<li class="experience vevent vcard">
+						<h4 class="title">Junior Member at EESTEC Bucharest</h4>
+						<h5 class="org summary"><a href="http://www.eestec.ro/" title="EESTEC">EESTEC</a></h5>
+						<p class="organization-details">(Non-Profit Organization Management industry)</p>
+						<p class="period">
+							<abbr class="dtstart" title="2008-11-05">November 2007</abbr> - <abbr class="dtstamp" title="2008-03-10">March 2008</abbr>
+						</p>
+						<p class="description">
+							As a junior member I have handled various tasks given by the senior members.
+						</p>
+					</li>
+				</ol>
+			</div>
+			<div id="education" class="container">
+				<h3>Education</h3>
+				<ol class="vcalendar">
+					<li class="education vevent vcard">
+						<h4 class="org">
+							<a href="http://www.soton.ac.uk" title="University of Southampton">University of Southampton</a>,
+							<a href="http://www.ecs.soton.ac.uk/" title="School of Electronics and Computer Science">School of Electronics and Computer Science</a>
+						</h4>
+						<p class="date"><abbr class="dtstart" title="2010-10-04">October 2010</abbr> - <abbr class="dtend" title="2011-09-30">September 2011</abbr></p>
+						<p class="description">
+							MSc, Software Engineering
+						</p>
+						<div class="modules">
+							<ul>
+								<li><a href="http://www.ecs.soton.ac.uk/admissions/pg/msc/syllabus.php?session=1011&amp;unit=COMP6008">Research Methods in Computing</a></li>
+								<li><a href="http://www.ecs.soton.ac.uk/admissions/pg/msc/syllabus.php?session=1011&amp;unit=COMP6013">Agile and Object Oriented Development</a></li>
+								<li><a href="http://www.ecs.soton.ac.uk/admissions/pg/msc/syllabus.php?session=1011&amp;unit=COMP6038">Safety Critical Systems</a></li>
+								<li><a href="http://www.ecs.soton.ac.uk/admissions/pg/msc/syllabus.php?session=1011&amp;unit=COMP6039">Software Project Management &amp; Quality Assurance</a></li>
+								<li><a href="http://www.ecs.soton.ac.uk/admissions/pg/msc/syllabus.php?session=1011&amp;unit=COMP6003">Enterprise Web Development</a></li>
+								<li><a href="http://www.ecs.soton.ac.uk/admissions/pg/msc/syllabus.php?session=1011&amp;unit=COMP6017">Advanced Topics on Web Services</a></li>
+								<li><a href="http://www.ecs.soton.ac.uk/admissions/pg/msc/syllabus.php?session=1011&amp;unit=COMP6004">Formal Design of Systems</a></li>
+								<li><a href="http://www.ecs.soton.ac.uk/admissions/pg/msc/syllabus.php?session=1011&amp;unit=COMP6035">From Aspect-Oriented Programming to Program Generation</a></li>
+								<li><a href="http://www.ecs.soton.ac.uk/admissions/pg/msc/syllabus.php?session=1011&amp;unit=COMP6051">Social Networking Technologies</a></li>
+							</ul>
+						</div>
+					</li>
+					<li class="education vevent vcard">
+						<h4 class="org">
+							<a href="http://education.oracle.com" title="Oracle University">Oracle University</a>
+						</h4>
+						<p class="date"><abbr class="dtstart" title="2009-07-13">July 2009</abbr> - <abbr class="dtend" title="2009-07-17">July 2009</abbr></p>
+
+						<p class="description">
+							I have attended the course "OracleAS 10g R3: Build Java EE Applications I Ed 2". Main topics and concepts:
+						</p>
+						<div class="modules">
+							<ul>
+								<li>JavaServer Faces</li>
+								<li>Enterprise JavaBeans 3.0</li>
+								<li>Oracle Toplink</li>
+								<li>Oracle Application Development Framework (ADF)</li>
+								<li>ADF Faces</li>
+								<li>Deploying applications to OC4J</li>
+								<li>Working with Oracle JDeveloper 10.1.3.2</li>
+							</ul>	
+						</div>
+					</li>
+					<li class="education vevent vcard">
+						<h4 class="org">
+							<a href="http://www.pub.ro" title='University "Politehnica" of Bucharest'>University "Politehnica" of Bucharest</a>,
+							<a href="http://acs.pub.ro" title="Automatic Control and Computers Faculty">Automatic Control and Computers Faculty</a>
+						</h4>
+						<p class="date"><abbr class="dtstart" title="2006-10-01">October 2006</abbr> - <abbr class="dtend" title="2010-07-14">July 2010</abbr></p>
+
+						<p class="description">
+							BEng, Automatic Control and Applied Informatics
+						</p>
+					</li>
+					<li class="education vevent vcard">
+						<h4 class="org">
+							Cisco Systems Networking Academy, Bucharest Academy of Economic Studies
+						</h4>
+						<p class="date"><abbr class="dtstart" title="2006-10-10">October 2006</abbr> - <abbr class="dtend" title="2007-01-14">January 2007</abbr></p>
+
+						<p class="description">
+							Java Programmer. Cisco Systems Networking Academy - Certificate of Course Completion - Fundamentals of Java Programming.
+						</p>
+					</li>
+					<li class="education vevent vcard">
+						<h4 class="org">
+							Oracle Internet Academy, "Cantemir Vodă" National College, Bucharest
+						</h4>
+						<p class="date"><abbr class="dtstart" title="2005-01-08">January 2005</abbr> - <abbr class="dtend" title="2005-06-09">June 2005</abbr></p>
+						<p class="description">
+							SQL Programmer. Introduction to 9i: SQL Certification Exam.
+						</p>
+					</li>
+				</ol>
+			</div>
+			<div id="publications" class="container">
+				<h3>Publications</h3>
+				<ol class="vcard">
+					<li>
+						<cite><a href="http://www.linuxidentity.com/us/index.php?name=News&amp;file=article&amp;sid=5048" title="Linux Identity Office – Ubunutu 10.10">Linux Identity Office (Volume four, number four) – Ubuntu 10.10</a></cite>, <a class="fn org url" href="http://www.oxypress.com/" title="OXY PRESS LLC">OXY PRESS LLC</a>, 2010.
+						<p class="description">I have contributed with the following articles:
+							<cite>What to do after you install Ubuntu?</cite>,
+							<cite>Online file storage - Dropbox vs. Ubuntu One</cite>
+						</p>
+					</li>
+					<li>
+						<cite><a href="http://www.linuxidentity.com/us/index.php?name=News&amp;file=article&amp;sid=5041" title="Linux Identity Office – OpenOffice.org 3.2">Linux Identity Office (Volume four, number two) – OpenOffice.org 3.2</a></cite>, <a class="fn org url" href="http://www.oxypress.com/" title="OXY PRESS LLC">OXY PRESS LLC</a>, 2010.
+						<p class="description">I have contributed with the following articles:
+							<cite>OpenOffice.org 3.2 - What's new?</cite>,
+							<cite>Installing OpenOffice.org 3.2</cite>,
+							<cite>Dictionaries in OpenOffice.org</cite>,
+							<cite>Math</cite>,
+							<cite>Macros and API</cite>,
+		                    <cite>Tips for Microsoft Office users migrating to OpenOffice.org</cite>.
+						</p>
+					</li>
+					<li>
+						<cite><a href="http://www.linuxidentity.com/us/index.php?name=News&amp;file=article&amp;sid=5038" title="Linux Identity Starter - Ubuntu Family 9.10 Karmic Koala">Linux Identity Starter (Volume two, number two) - Ubuntu Family 9.10 Karmic Koala</a></cite>, <a class="fn org url" href="http://www.oxypress.com/" title="OXY PRESS LLC">OXY PRESS LLC</a>, 2009.
+						<p class="description">I have contributed with the following articles:
+							<cite>DVD Contents</cite>,
+							<cite>9.10 vs. 9.04</cite>,
+							<cite>Installing Ubuntu</cite>,
+							<cite>Ubuntu directory structure</cite>,
+							<cite>Virtualization under Ubuntu</cite>.
+						</p>
+					</li>
+					<li>
+						<cite><a href="http://www.linuxidentity.com/us/index.php?name=News&amp;file=article&amp;sid=5037" title="Linux Identity Office - Linux Starter Kit (Ubuntu 9.04)">Linux Identity Office (Volume three, number two) - Linux Starter Kit (Ubuntu 9.04)</a></cite>, <a class="fn org url" href="http://www.oxypress.com/" title="OXY PRESS LLC">OXY PRESS LLC</a>, 2009.
+						<p class="description">I have contributed with the following articles:
+							<cite>Editorial</cite>,
+							<cite>Why Ubuntu?</cite>,
+							<cite>CD content</cite>,
+							<cite>Installing Ubuntu</cite>,
+							<cite>Dual-booting Ubuntu 9.04 with Windows</cite>,
+							<cite>Wubi</cite>,
+							<cite>Software configuration</cite>,
+							<cite>Hardware configuration</cite>,
+							<cite>Ubuntu directory structure</cite>,
+							<cite>Graphical environments</cite>,
+							<cite>Cool Ubuntu applications</cite>,
+							<cite>Installing multimedia codecs &amp; video drivers</cite>,
+							<cite>Communication messengers</cite>,
+							<cite>Bluetooth</cite>,
+							<cite>Installation of webcams</cite>,
+							<cite>Nautilus scripts</cite>,
+							<cite>Ubuntu terminal command guide</cite>,
+							<cite>Tips &amp; tricks</cite>,
+							<cite>FAQ - Frequently Asked Questions</cite>.
+						</p>
+					</li>
+				</ol>
+			</div>
+		</div>
+	</div>
+	<div class="bottom">
+		<a href="http://microformats.org/wiki/hresume" title="Microformats - hResume">
+		<img src="logo.gif" alt="Microformats - hResume" title="Microformats - hResume" />
+		</a>
+	</div>
+	<script type="text/javascript" src="noSpam.js"></script>
+</body>
+</html>
