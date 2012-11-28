342abe3bb5a94ba11cd79197e5de5e6e4d898240
Radu Cotescu <radu@cotescu.com>
2012-11-28 14:18:34 +0200

    Site updated at 2012-11-28 12:18:34 UTC

--- resume/noSpam.js
+++
@@ -1,8 +0,0 @@
-function noSpam1(user, domain) {
-	document.location = 'mailto:' + user + '@' + domain;
-	}
-
-function noSpam2(user, domain, subject) {
-	document.location = 'mailto:' + user + '@' + domain + '?subject=' + subject;
-	}
-
